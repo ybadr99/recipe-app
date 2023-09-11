2bf441ac2c37d5b74b8c5847df07f7e712a24c36
cherelemma <chere.lemma@aastu.edu.et>
2023-09-11 22:10:42 +0300

    create recipes route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+
+  resources :recipes do
+    resources :recipe_foods, only: [:create, :destroy]
+  end
 end
