e75e5c0a213ea62638ca8c8e2a6f76752c0ccd3c
cherelemma <chere.lemma@aastu.edu.et>
2023-09-12 20:17:09 +0300

    add association between recipes and recipe_foods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_foods, class_name: 'RecipeFood', dependent: :destroy
+  has_and_belongs_to_many :foods, join_table: :recipe_foods
+  
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
