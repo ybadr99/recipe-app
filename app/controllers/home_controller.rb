63043e884daf03cae7ac2c90da63d18468c8d03d
cherelemma <chere.lemma@aastu.edu.et>
2023-09-12 19:03:10 +0300

    add a modal controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def index; end
+
+  def new;  end
 end
---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,21 @@
+// app/javascript/controllers/modal_controller.js
+
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.modal = new bootstrap.Modal(this.element)
+  }
+
+  open() {
+    if (!this.modal.isOpened) {
+      this.modal.show()
+    }
+  }
+
+  close(event) {
+    if (event.detail.success) {
+      this.modal.hide()
+    }
+  }
+}
