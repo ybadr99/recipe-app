cfbe7c0ef77f842c402156a724da75901f2d48a7
cherelemma <chere.lemma@aastu.edu.et>
2023-09-13 18:45:53 +0300

    fix linter erros with rubocop

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
-class ApplicationSystemTestCase < ActionDispatch: :systemtestcase;
-driven_by: selenium,;
-using: :chrome,;
-screen_size:
-  [1400,
-  1400]
-  end;
+class ApplicationSystemTestCase < ActionDispatch.systemtestcase
+  driven_by :selenium,
+            using: :chrome,
+            screen_size:
+              [1400,
+               1400]
+end
